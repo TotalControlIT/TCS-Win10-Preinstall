eef5b9e8b81f7d3f1d59565a268c9915c63ce540
Carl Hopkins <mail@carlhopkins.com>
2024-03-28 10:34:32 +0000

    Forked from source

---
+++ TCSWin10Prep.bat
@@ -0,0 +1,9 @@
+@echo off 
+REM # TCS Windows 10 Machine Pre-Installation PreP Tool - Loader - v0.11 - Copyright (c) 2023 Carl Hopkins
+
+ECHO +====================================================+
+ECHO +         TCS Windows 10 PreP Loader - v0.11         +
+ECHO +====================================================+
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""iwr -useb https://raw.githubusercontent.com/bitterblitter/TCS-Win10-Preinstall/main/TCSWin10Prep.ps1 | iex""' -Verb RunAs}"
+TIMEOUT /T 2
